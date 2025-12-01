bdc87443c128c52934774c84ba407e55db5ff4f8
kl3266 <kl3266@columbia.edu>
2025-12-01 16:54:07 -0500

    3. Add software stack and utils

---
+++ soft/common/apps/baremetal/3_by_3_chiplet_p2p_2_unicasts/3_by_3_chiplet_p2p_2_unicasts.h
@@ -0,0 +1,14 @@
+/* Copyright (c) 2011-2021 Columbia University, System Level Design Group */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __3_BY_3_CHIPLET_P2P_2_UNICASTS_H__
+#define __3_BY_3_CHIPLET_P2P_2_UNICASTS_H__
+
+#ifdef __riscv
+#define ACC_BASE_ADDR 0x60010000
+#else
+#define ACC_BASE_ADDR 0x80010000
+#endif
+#define ACC_OFFSET 0x200
+
+#endif /* __3_BY_3_CHIPLET_P2P_2_UNICASTS_H__ */
---
+++ soft/common/drivers/common/include/esp_accelerator.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2011-2024 Columbia University, System Level Design Group
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __ESP_ACCELERATOR_H__
+#define __ESP_ACCELERATOR_H__
+
+#define BIT(nr)			(1UL << (nr))
+
+#ifndef __KERNEL__
+#define __round_mask(x, y) ((y)-1)
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+#endif
+
+
+/***
+ * ESP accelerator common definitions and registers offset
+ ***/
+
+/* bank(0): CMD (reset if cleared) */
+#define CMD_REG 0x00
+#define CMD_MASK_START BIT(0)
+
+/* bank(1): STATUS (idle when cleared) - Read only */
+#define STATUS_REG 0x04
+#define STATUS_MASK_RUN  BIT(0)
+#define STATUS_MASK_DONE BIT(1)
+#define STATUS_MASK_ERR  BIT(2)
+
+/* bank(2)        : RESERVED - Read only */
+#define DEVID_REG 0x08
+
+/* bank(3)        : PT_ADDRESS (page table bus address) */
+#define PT_ADDRESS_REG 0x0c
+
+/* bank(4)        : PT_NCHUNK (number of physical contiguous buffers in memory) */
+#define PT_NCHUNK_REG 0x10
+
+/* bank(5)        : PT_SHIFT (log2(cunk size)) */
+#define PT_SHIFT_REG 0x14
+
+/* bank(6)        : PT_NCHUNK_MAX (maximum number of chunks supported) - Read only */
+#define PT_NCHUNK_MAX_REG 0x18
+
+/* bank(7)        : PT_ADDRESS_EXTENDED (page table bus address MSBs) */
+#define PT_ADDRESS_EXTENDED_REG 0x1c
+
+/* bank(8)        : Type of coherence (None, LLC, Full) - Read only */
+#define COHERENCE_REG 0x20
+enum accelerator_coherence {ACC_COH_NONE = 0, ACC_COH_LLC, ACC_COH_RECALL, ACC_COH_FULL, ACC_COH_AUTO};
+
+/* bank(9)       : Point-to-point configuration */
+#define P2P_REG 0x24
+#define P2P_MASK_NSRCS 0x3
+#define P2P_MASK_SRC_IS_P2P BIT(2)
+#define P2P_MASK_DST_IS_P2P BIT(3)
+#define P2P_MASK_SRCS_YX 0xF  // changed from 0x7 to 0xF
+#define P2P_MASK_SRCS_CHIP_YX 0x7
+#define P2P_MASK_MCAST_NDESTS 0xF
+#define P2P_BIT_SRCS_YX 4       // start index is 4
+#define P2P_BIT_SRCS_CHIP_YX 0  // start index is 0
+#define YX_WIDTH 4
+#define CHIP_YX_WIDTH 3
+#define P2P_SHIFT_SRCS_Y(_n) (P2P_BIT_SRCS_YX + YX_WIDTH + _n * 2 * YX_WIDTH)
+#define P2P_SHIFT_SRCS_X(_n) (P2P_BIT_SRCS_YX + _n * 2 * YX_WIDTH)
+#define P2P_SHIFT_SRCS_CHIP_Y(_n) (P2P_BIT_SRCS_CHIP_YX + CHIP_YX_WIDTH + _n * 2 * CHIP_YX_WIDTH)
+#define P2P_SHIFT_SRCS_CHIP_X(_n) (P2P_BIT_SRCS_CHIP_YX + _n * 2 * CHIP_YX_WIDTH)
+#define P2P_SHIFT_MCAST_NDESTS 28
+
+/* bank(10)       : Point-to-point chip configuration */
+#define P2P_CHIP_REG 0x28
+
+/* bank(11)       : SRC_OFFSET (offset in bytes from beginning of physical buffer) */
+#define SRC_OFFSET_REG 0x2C
+
+/* bank(12)       : DST_OFFSET (offset in bytes from beginning of physical buffer) */
+#define DST_OFFSET_REG 0x30
+
+/* bank(13)       : RESERVED */
+#define SPANDEX_REG 0x34
+
+/* YX_REGs contain a mapping from an accelerator number to a physical tile coordinate */
+/* bank(14)       : YX_REG - LSBs reserved for local tile's coordinates */
+#define YX_REG 0x38
+#define YX_SHIFT_X 0
+#define YX_SHIFT_Y 4
+#define YX_MASK_YX 0xF
+#define YX_WIDTH 4
+
+/* bank(15)       : YX_REG_2 */
+#define YX_REG_2 0x3C
+
+/* bank(16)       : YX_REG_3 */
+#define YX_REG_3 0x40
+
+/* bank(17)       : CHIP_YX_REG */
+#define CHIP_YX_REG 0x44
+#define CHIP_YX_SHIFT_CHIP_X 0
+#define CHIP_YX_SHIFT_CHIP_Y 3
+#define CHIP_YX_MASK_CHIP_YX 0x7
+#define CHIP_YX_WIDTH 3
+
+/* bank(18)       : CHIP_YX_REG_2 */
+#define CHIP_YX_REG_2 0x48
+
+/* bank(19)       : CHIP_YX_REG_3 */
+#define CHIP_YX_REG_3 0x4C
+
+
+/* bank(20 to 63) : USR (user defined) */
+
+// Re-enable the following 3 registers if adding an SRAM expanding the register bank
+// /* bank(29)       : EXP_ADDR (bits 29:0 address an SRAM expanding the register bank) */
+// #define EXP_ADDR_REG 0x74
+// #define EXT_MASK_R BIT(30)
+// #define EXT_MASK_W BIT(31)
+// /* bank(30)       : EXP_DI (data to be written to the expansion SRAM) */
+// #define EXP_DI_REG 0x78
+// /* bank(31)       : EXP_DO (data read from the exansion SRAM) */
+// #define EXP_DO_REG 0x7c
+
+#endif /* __ESP_ACCELERATOR_H__ */
